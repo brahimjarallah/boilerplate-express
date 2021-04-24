52e79c0b145ddc18b1fa957bc02f771e20c88918
brahim.jarallah <brahim.jarallah@gmail.com>
2021-04-24 17:14:19 +0100

    express log

--- myApp.js
+++ myApp.js
@@ -1,39 +1,33 @@
-var express = require('express');
-var app = express();
+var express = require("express")
+var app = express()
+var bGround = require("fcc-express-bground")
 
+module.exports = app
 
+// Meet the Node console
+bGround.log("Hello World")
+console.log("Hello World")
 
+// Start a Working Express Server
 
+// Serve an HTML File
 
+// Serve Static Assets
 
+// Serve JSON on a Specific Route
 
+// Use the .env File
 
+// Implement a Root-Level Request Logger Middleware
 
+// Chain Middleware to Create a Time Server
 
+// Get Route Parameter Input from the Client
 
+// Get Query Parameter Input from the Client
 
+// Use body-parser to Parse POST Requests
 
+// Get Data from POST Requests
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
- module.exports = app;
+module.exports = app
